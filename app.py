d51866a6446a110aa3e0d4296a4aa3fb03293927
jameis <60635127+jameis@users.noreply.github.com>
2023-06-02 10:07:41 -0700

    Update app.py

--- app.py
+++ app.py
@@ -2,3 +2,29 @@ import streamlit as st
 st.title('My CE App') # Sets the the title of your page
 st.header('Data Visualization Section')  # Sets a header for a section
 st.subheader('Subsection: Pie Chart Analysis')  # Sets a subheader for a subsection
+st.text('This section focuses on data preprocessing.')
+st.markdown('**This is some bold text**')
+
+import pandas as pd
+
+data = {
+    'A': [45, 37, 89, 67],
+    'B': ['John', 'Max', 'Lisa', 'Chris']
+}
+df = pd.DataFrame(data)
+
+st.write(df)
+
+age = st.slider('Your age', min_value=0, max_value=100)  # Returns the value selected by the user
+if st.button('Say Hello'):  # Returns True if the user clicks the button
+    st.write('Hello!')
+show_df = st.checkbox('Show DataFrame')  # Returns True if the user checks the box, False otherwise
+if show_df:
+    st.write(df)
+    
+    age = st.slider('Your age', min_value=0, max_value=100)  # Returns the value selected by the user
+if st.button('Say Hello'):  # Returns True if the user clicks the button
+    st.write('Hello!')
+show_df = st.checkbox('Show DataFrame')  # Returns True if the user checks the box, False otherwise
+if show_df:
+    st.write(df)
